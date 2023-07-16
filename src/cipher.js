8456e5922464cec6e9c0838d95a76b73292343c3
Sara Anita <sara.anita.desa@gmail.com>
2023-07-15 23:32:11 -0300

    Modificando o Layout ainda não é o definitivo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,19 @@
-const cipher = {
-  // ...
-};
+// const cipher = {
+//   // ...
+// };
 
-export default cipher;
+// export default cipher;
+
+// . ==> class
+// # ==> id
+// div ==> element
+
+
+function codificar() {
+  document.querySelector('#codificar').style.color = "red";
+}
+
+const botao = document.querySelector('#codificar')
+botao.addEventListener('click', codificar)
+
+
