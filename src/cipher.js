6484eb4b74e72685dd55f5ed497aea7729ad78a6
Sara Anita <sara.anita.desa@gmail.com>
2023-07-24 23:20:00 -0300

    Fazendo Ajustes na validação do Js

--- src/cipher.js
+++ src/cipher.js
@@ -1,46 +1,80 @@
 const cipher = {
-  encode:(mensagem) => {
-    
-    let mensagemInserida = "";
-  
-    for (let i = 0; i < mensagem.length; i++) {
-      let charCode = mensagem.charCodeAt(i);
-      // ASC II usado como referencia.
-      // o descolamento escolhido foi 2
-      // A -> C; a -> c
-      // o %26 está ali pra garantir que o looping fique entre as 26 letras do alfabeto
-      // considerando que a letra a está na posição 0
-  
-      // //Letras Maiusculas
-      // if (charCode >= 65 && charCode <= 90){
-      //   charCode = ((charCode - 65 + 2)% 26) + 65;
-  
-      // //Letras Minusculas
-      // } else if (charCode >= 97 && charCode <= 122) {
-      //   charCode = ((charCode - 97 + 2) % 26) + 97;
-      // }
-  
-      // resolvi fazer com o alfabeto todo para incluir palavras com acento na codificação
-      //pesquisar typeof / const mensagem = document.getElementById("mensagem").value;
-      charCode = (charCode + 2) % 255
-      mensagemInserida += String.fromCharCode(charCode);
-    }
-    document.getElementById("mensagemCifrada").textContent = mensagemInserida;
+  encode: (mensagem, descolamento) => {
+    try {
+      
+      const mensagemSemEspaco = mensagem.trim();
 
-  },
-  decode:() => {
-    const decodificando = document.getElementById("decodificando").value;
-    let mensagemDescifrada = "";
-  
-    for (let i = 0; i < decodificando.length; i++) {
-      let charCode = decodificando.charCodeAt(i);
-      charCode = (charCode - 2) % 255;
-      mensagemDescifrada += String.fromCharCode(charCode);
+      let mensagemInserida = "";
+      let mensagemValida = true;
+      
+      for (let i = 0; i < mensagemSemEspaco.length; i++) {
+        let charCode = mensagemSemEspaco.charCodeAt(i);
+
+        // ASC II usado como referencia.
+        // o descolamento escolhido foi 2
+        // A -> C; a -> c
+        // o %26 está ali pra garantir que o looping fique entre as 26 letras do alfabeto
+        // considerando que a letra a está na posição 0
+
+        if (charCode >= 65 && charCode <= 90 || charCode === 32) {
+          charCode = ((charCode - 65 + descolamento) % 26) + 65;
+        } else {
+          mensagemValida = false;
+          break;
+        }
+
+        mensagemInserida += String.fromCharCode(charCode);
+      }
+
+      // Verifica se a mensagem é válida antes de atribuí-la ao elemento HTML.
+      if (!mensagemValida) {
+        throw new TypeError('Por favor, Insira um Caracter de A - Z', 'cipher.js', '25');
+      }
+
+      document.getElementById("mensagemCifrada").textContent = mensagemInserida;
+    } catch (error) {
+      // Aqui, no bloco "catch", trata o erro.
+      alert('Erro');
     }
-  
-    document.getElementById("mensagemDescifrada").textContent = mensagemDescifrada;
-    
+  },
+
+  decode: (decodificando, descolamento) => {
+    try {
+
+      const decoSemEspaco = decodificando.trim();
+
+      let mensagemDecifrada = ""; // Alterado para "let" em vez de "const"
+      let mensagemVazia = true;
+
+      if (!decoSemEspaco) {
+        return window.alert('Por favor, insira uma mensagem');
+      }
+
+      for (let i = 0; i < decoSemEspaco.length; i++) {
+        let charCode = decoSemEspaco.charCodeAt(i);
+        if (charCode >= 65 && charCode <= 90 || charCode === 32) {
+          charCode = ((charCode - 65 - descolamento) % 26) + 65;
+        } else {
+          mensagemVazia = false;
+          break;
+        }
+
+        mensagemDecifrada += String.fromCharCode(charCode);
+      }
+
+      // Verifica se a mensagem foi decifrada corretamente antes de atribuí-la ao elemento HTML.
+      if (!mensagemVazia) {
+        throw new TypeError('Por favor, Insira um Caracter de A - Z', 'cipher', '49');
+      }
+
+      document.getElementById("mensagemDescifrada").textContent = mensagemDecifrada;
+    } catch (error) {
+      // Aqui, no bloco "catch", tratamos o erro.
+      // Exibimos uma mensagem de erro na tela para o usuário.
+      alert('Erro');
+    }
   }
-}
+};
+
 
 export default cipher
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import cipher from './cipher.js';
 
-document.getElementById("btnCifrar").addEventListener("click", cipher.encode);
-document.getElementById("btnDecifrar").addEventListener("click", cipher.decode);
+document.getElementById("cifrar").addEventListener("submit", pegarMensagem);
 
-// const mensagem = document.getElementById("mensagem").value;
+document.getElementById("decifrar").addEventListener("submit", pegarMensagemDec);
+
+
+function pegarMensagem() {
+  event.preventDefault()
+  const mensagem = document.getElementById("mensagem").value;
+  cipher.encode(mensagem)
+  const deslocamento = parseInt(document.getElementById("deslocamento").value);
+  cipher.encode(mensagem, deslocamento);
+
+}
+
+function pegarMensagemDec(){
+  event.preventDefault()
+  const decodificando = document.getElementById("decodificando").value;
+  cipher.decode(decodificando)
+  const deslocamento = parseInt(document.getElementById("deslocamento").value);
+  cipher.decode(decodificando, deslocamento);
+}
+
 
 // criar função para tirar o mensagem da parte cipher 
