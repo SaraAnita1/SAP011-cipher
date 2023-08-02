f5ac0b7cc65c163518b86ab86e10401d6ca7ac83
Sara Anita <sara.anita.desa@gmail.com>
2023-08-02 00:27:49 -0300

    Updates

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 import cipher from './cipher.js';
 
-document.getElementById("cifrar").addEventListener("submit", pegarMensagem);
+document.querySelector('form').addEventListener('submit', validarForm)
 
-document.getElementById("decifrar").addEventListener("submit", pegarMensagemDec);
+function validarForm(){
+  event.preventDefault()
+
+  if(document.querySelector('form').className === 'cifrar'){
+    pegarMensagem()
+  } else {
+    pegarMensagemDec()
+  } 
+}
+
 
 
 function pegarMensagem() {
-  event.preventDefault()
   try { 
     const mensagem = document.getElementById("mensagem").value;
     const deslocamento = parseInt(document.getElementById("deslocamento").value);
     const mensagemCifrada = cipher.encode(deslocamento, mensagem);
-    document.getElementById("mensagemCifrada").textContent = mensagemCifrada;
+    document.getElementById("mensagemCifrada").textContent = mensagemCifrada; 
+    // localStorage.setItem('mensagemCifrada', mensagemCifrada)
   } catch (error) {
     alert('Erro ao codificar' + error.message)
   }
 }
 
 function pegarMensagemDec(){
-  event.preventDefault()
   try{
-    const decodificando = document.getElementById("decodificando").value;
+    const decodificando = document.getElementById("mensagem").value;
     const deslocamento = parseInt(document.getElementById("deslocamento").value);
     const mensagemDec = cipher.decode(deslocamento, decodificando);
-    document.getElementById("mensagemDescifrada").textContent = mensagemDec;
+    document.getElementById("mensagemCifrada").textContent = mensagemDec;
   } catch (error) {
     alert('Erro ao decodificar' + error.message)
   }
@@ -34,17 +42,36 @@ function pegarMensagemDec(){
 const trocaMetodo = document.querySelector('.trocaMetodo')
 
 trocaMetodo.addEventListener('click', function(){
-  const formCifrar = document.querySelector('#cifrar')
-  const gif = document.querySelector('.gifContainer')
-  const formDescifrar = document.querySelector('#decifrar')
 
-  formCifrar.classList.toggle('oculto')
-  gif.classList.toggle('oculto')
-  
-  setTimeout(function(){
-    gif.classList.toggle('oculto')
-    formDescifrar.classList.toggle('oculto')
-  }, 3000)
+  const form = document.querySelector("form")
+  document.querySelector('p').innerHTML = ''
+  document.querySelector('textarea').value = ''
+  form.classList.toggle("cifrar")
+  form.classList.toggle("decifrar")
+
+  if(form.className === "decifrar"){
+    document.querySelector('form button').innerHTML = 'Decodificar'
+    document.querySelector('.trocaMetodo').innerHTML = 'Deseja Codificar ?'
+  } else {
+    document.querySelector('form button').innerHTML = 'Codificar'
+    document.querySelector('.trocaMetodo').innerHTML = 'Deseja Decodificar ?'
+  }
+
+  loading()
 })
+
+function loading(){
+  document.querySelector('.des').classList.add('oculto')
+  document.querySelector('form').classList.add('oculto')
+  document.querySelector('.trocaMetodo').classList.add('oculto')
+  document.querySelector('figure').classList.remove('oculto')
+
+  setTimeout(function(){
+    document.querySelector('.des').classList.remove('oculto')
+    document.querySelector('form').classList.remove('oculto')
+    document.querySelector('.trocaMetodo').classList.remove('oculto')
+    document.querySelector('figure').classList.add('oculto')
+  }, 3000)
+}
 
 
